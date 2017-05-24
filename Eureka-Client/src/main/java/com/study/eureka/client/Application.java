cb2c8304515e2887fb3f2263067574fee24f95a8
mason <mason.yu@emc.com>
2017-05-24 14:18:04 +0800

    Spring cloud project

---
+++ Eureka-Client/src/main/java/com/study/eureka/client/Application.java
@@ -0,0 +1,24 @@
+package com.study.eureka.client;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableAutoConfiguration
+public class Application {
+	
+	@RequestMapping("/test")
+    public String home() {
+        return "Hello world";
+    }
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+}
